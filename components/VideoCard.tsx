c424764dc91bd9cab05d761013ca6d6bd5bbbe59
haven rutter <108587591+havenbeats1@users.noreply.github.com>
2022-09-29 14:17:02 -0700

    commit

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -83,7 +83,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                         </video>
                     </Link>
                     {isHover && (
-                        <div>
+                        <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between">
                             {playing ? (
                                 <button onClick={onVideoPress}>
                                     <BsFillPauseFill className='text-black text-2xl lg:text-4xl'/>
@@ -94,11 +94,11 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                                 </button>
                             )}
                             {isVideoMuted ? (
-                                <button>
+                                <button onClick={() => setIsVideoMuted(false)}>
                                     <HiVolumeOff className='text-black text-2xl lg:text-4xl'/>
                                 </button>
                             ) : (
-                                <button>
+                                <button onClick={() => setIsVideoMuted(true)}>
                                     <HiVolumeUp className='text-black text-2xl lg:text-4xl'/>
                                 </button>
                             )}
